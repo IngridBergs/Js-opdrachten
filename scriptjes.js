039ac7c86b1d31b322bfaafd51eb1a4ed38edf49
IngridBergs <ingrid.bergs@gmail.com>
2018-12-12 09:09:01 +0100

    oef 1 t/m 13

---
+++ scriptjes.js
@@ -0,0 +1,20 @@
+var number_1;
+var number_2;
+
+
+function calcModulo(number_1,number_2) {
+
+    return number_1%number_2;
+}
+
+
+
+
+
+
+var length = prompt("Was is je lengte aub?: ");
+var birthDate = prompt ("Wat is je geboortejaar aub?: ");
+
+function calcVaria(length,birthDate){
+    return ((length*2)+5)-birthDate+1766;
+}
